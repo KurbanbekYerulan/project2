1525c2824ee3f3ba0cb3cfa1e30ebf902ec1e857
yerulankurbanbek <kurbanbek.yerulan@mail.ru>
2022-10-17 16:14:20 +0600

    project started

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,7 +59,7 @@
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Did you your Password?') }}
                                     </a>
                                 @endif
                             </div>
